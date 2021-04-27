a71ec564510f9097e5204183e52fcaea5a968cad
cryptic-peanut <bartekddd@op.pl>
2021-04-27 02:36:43 +0200

    Dodaj pliki projektów.

---
+++ Middleware/BrowserCheck.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class BrowserCheck
+    {
+        private RequestDelegate _next;
+        public BrowserCheck(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public Task Invoke(HttpContext context)
+        {
+            var browserInfo = context.Request.Headers["User-Agent"].ToString();
+            Debug.WriteLine("Browser: " + browserInfo);
+            /*if (context.Request.Path == "/")
+                context.Request.Path = "/Privacy";*/
+            return _next(context);
+        }
+    }
+
+    public static class BrowserCheckExtensions
+    {
+        public static IApplicationBuilder BrowserCheck(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowserCheck>();
+        }
+    }
+}
